1942beb71face5ad8978431742243dc965068781
sawft99 <81699231+sawft99@users.noreply.github.com>
2021-12-05 11:50:36 -0500

    Changed exmaple
    Updated example for clarity.

--- ProfileCopy.bat
+++ ProfileCopy.bat
@@ -1,5 +1,5 @@
 @echo off
-set /p Share="Enter share (Ex. Fileserver.domain.local\Users): "
+set /p Share="Enter share (Ex. Fileserver.domain.local\Users\Bob): "
 set /p User="Enter local username to copy: "
 robocopy /ZB /NOCOPY /MIR /XJ /MT:16 /R:3 /W:3 /TBD /V /ETA "C:\Users\%User%" "\\%Share%"
 echo.
