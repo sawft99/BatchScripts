b3c15419b9e447fdf5fd95efd09db45601ba5407
sawft99 <81699231+sawft99@users.noreply.github.com>
2022-02-05 11:33:35 -0500

    Format/Logic change
    Update to format for consistency and readability. Fixed some logic errors.

--- Cleanup.bat
+++ Cleanup.bat
@@ -3,7 +3,7 @@
 @echo off
 color 0a
 setlocal enableextensions
-SETLOCAL
+setlocal
 pushd %SystemDrive%
 Title Cleanup Script...
 set starttime=%TIME%
@@ -19,15 +19,17 @@ if %errorLevel% == 1 (
 	echo Doesn't look like you're running this as a local admin...
 	echo.
 	set /p confirm="Continue anyway? Failures will likely occur otherwise unless you are sure.(Y,N): "
-	if "%confirm%"=="yes" goto start else (goto wronginput)
-	if "%confirm%"=="y" goto start else (goto wronginput)
-	if "%confirm%"=="YES" goto start else (goto wronginput)
-	if "%confirm%"=="Y" goto start else (goto wronginput)
-	if "%confirm%"=="no" goto noanswer else (goto wronginput)
-	if "%confirm%"=="NO" goto noanswer else (goto wronginput)
-	if "%confirm%"=="n" goto noanswer else (goto wronginput)
-	if "%confirm%"=="N"goto noanswer else (goto wronginput)
-) else ( goto wronginput )
+	if "%confirm%"=="yes" (goto start) else (goto wronginput)
+	if "%confirm%"=="y" (goto start) else (goto wronginput)
+	if "%confirm%"=="YES" (goto start) else (goto wronginput)
+	if "%confirm%"=="Y" (goto start) else (goto wronginput)
+	if "%confirm%"=="no" (goto noanswer) else (goto wronginput)
+	if "%confirm%"=="NO" (goto noanswer) else (goto wronginput)
+	if "%confirm%"=="n" (goto noanswer) else (goto wronginput)
+	if "%confirm%"=="N" (goto noanswer) else (goto wronginput)
+) else (
+	goto wronginput
+)
 
 if %errorlevel% == 0 goto start
 
@@ -47,7 +49,7 @@ exit
 	
 :start
 cls
-echo  Starting cleanup ...
+echo  Starting cleanup...
 echo  --------------------------
 echo.
 echo Cleaning old windows files from upgrades and updates...
@@ -56,14 +58,14 @@ echo.
 
 :updateconfirmstage
 set /p updateconfirm="Clear windows updates cache (SoftwareDistribution folder)? Don't if updates are queued to be installed...(Y,N): "
-	if "%updateconfirm%"=="yes" goto updatecleanup else (goto wrongupdateinput)
-	if "%updateconfirm%"=="y" goto updatecleanup else (goto wrongupdateinput)
-	if "%updateconfirm%"=="YES" goto updatecleanup else (goto wrongupdateinput)
-	if "%updateconfirm%"=="Y" goto updatecleanup else (goto wrongupdateinput)
-	if "%updateconfirm%"=="no" goto therest else (goto wrongupdateinput)
-	if "%updateconfirm%"=="NO" goto therest else (goto wrongupdateinput)
-	if "%updateconfirm%"=="n" goto therest else (goto wrongupdateinput)
-	if "%updateconfirm%"=="N"goto therest else (goto wrongupdateinput)
+	if "%updateconfirm%"=="yes" (goto updatecleanup) else (goto wrongupdateinput)
+	if "%updateconfirm%"=="y" (goto updatecleanup) else (goto wrongupdateinput)
+	if "%updateconfirm%"=="YES" (goto updatecleanup) else (goto wrongupdateinput)
+	if "%updateconfirm%"=="Y" (goto updatecleanup) else (goto wrongupdateinput)
+	if "%updateconfirm%"=="no" (goto therest) else (goto wrongupdateinput)
+	if "%updateconfirm%"=="NO" (goto therest) else (goto wrongupdateinput)
+	if "%updateconfirm%"=="n" (goto therest) else (goto wrongupdateinput)
+	if "%updateconfirm%"=="N" (goto therest) else (goto wrongupdateinput)
 
 :wrongupdateinput
 echo.
@@ -82,38 +84,37 @@ if exist "%systemdrive%\windows\SoftwareDistribution\Download" (
 	Del /f /s /q "%systemdrive%\windows\SoftwareDistribution\Download\*"
 	rmdir /s /q "%systemdrive%\windows\SoftwareDistribution\Download\"
 	net start wuauserv
-	) else (
-		echo.
-		echo No such folder exists, moving on...
-		echo.
-		goto therest)
+) else (
+	echo.
+	echo No such folder exists, moving on...
+	echo.
+	goto therest
+)
 		
 :therest
+
 :: Previous Windows versions cleanup.
 if exist %SystemDrive%\Windows.old\ (
 	takeown /F %SystemDrive%\Windows.old /R /A /D Y
-	echo y| cacls %SystemDrive%\Windows.old\*.* /T /grant %userdomain%\%username%:F
+	echo y | cacls %SystemDrive%\Windows.old\*.* /T /grant %userdomain%\%username%:F
 	rmdir /S /Q %SystemDrive%\Windows.old
-	)
 )
 if exist %SystemDrive%\$Windows.~BT\ (
 	takeown /F %SystemDrive%\$Windows.~BT /R /A /D Y
-	echo y| cacls %SystemDrive%\Windows.old\*.*/T /grant %userdomain%\%username%:F
+	echo y | cacls %SystemDrive%\Windows.old\*.*/T /grant %userdomain%\%username%:F
 	rmdir /S /Q %SystemDrive%\$Windows.~BT\*.*
-	)
 )
 if exist %SystemDrive%\$Windows.~WS (
 	takeown /F %SystemDrive%\$Windows.~WS\* /R /A /D Y
-	echo y| cacls %SystemDrive%\Windows.old\*.* /T /grant %userdomain%\%username%:F
+	echo y | cacls %SystemDrive%\Windows.old\*.* /T /grant %userdomain%\%username%:F
 	rmdir /S /Q %SystemDrive%\$Windows.~WS\
-	)
 )
-
 echo.
 echo Done...
 echo.
 echo Cleaning up all users appdata/temp files...
 echo.
+
 :: Appdata cleanup (Chrome, IE, Flash, Java)
 if /i "%WIN_VER:~0,9%"=="Microsoft" (
 	for /D %%x in ("%SystemDrive%\Documents and Settings\*") do (
@@ -136,6 +137,7 @@ if /i "%WIN_VER:~0,9%"=="Microsoft" (
 		del /F /S /Q "%%x\My Documents\*.tmp" 2>NUL
 		del /F /S /Q "%%x\Recent\*" 2>NUL
 	)
+)
 ) else (
 	for /D %%x in ("%SystemDrive%\Users\*") do ( 
 		del /F /S /Q "%%x\*.blf" 2>NUL
@@ -164,11 +166,12 @@ if /i "%WIN_VER:~0,9%"=="Microsoft" (
 )
 
 :: Firefox cleanup
-if exist "%systemdrive%\Users\" for /D %%a in ("%systemdrive%\Users\*") do (
+if exist "%systemdrive%\Users\" for /D %%a in ("%systemdrive%\Users\*") (
 	for /D %%b in ("%%a\AppData\Local\Mozilla\Firefox\Profiles\*") do (
 		del /F /S /Q "%%b\cache2\entries\*"
 	)
 )
+
 :: Firefox cleanup 2
 for /D %%a in ("%systemdrive%\Users\*") do (
 	for /D %%b in ("%%a\AppData\Roaming\Mozilla\Firefox\Profiles\*") do (
@@ -177,6 +180,7 @@ for /D %%a in ("%systemdrive%\Users\*") do (
 REM		del /F /S /Q "%%b\*" (Will eat everything and reset all settings in Firefox)
 	)
 )
+
 :: IE Cleanup/reset
 RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 16
 RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 2
@@ -185,48 +189,52 @@ RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 8
 :: Win 10 Edge cleanup (WIP)
 REM echo %WIN_VER% | findstr /i /c:"Version 10.*"
 REM if not %ERRORLEVEL%==0 for /D %%a in (%systemdrive%\Users\*) do (
-REM for /D %%a in ("%systemdrive%\Users\*") do (
+REM 	for /D %%a in ("%systemdrive%\Users\*") do (
 REM		for /D %%b in ("%%a\AppData\Local\Packages\Microsoft.MicrosoftEdge*") do (
 REM			for /D %%c in ("%%b\AC") do (
-REM			del "%%c\*"
-REM			rmdir "%%c\*"
+REM				del "%%c\*"
+REM				rmdir "%%c\*"
 REM			)
 REM		)
 REM	)
 REM )
-
 echo.
 echo. Done.
 echo.
 echo   Cleaning other system temp files...
 echo.
 
-
 :: Remove everything in downloads
 REM FOR /i %%x in %systemdrive%\users\* do (
-REM 	del %%x\downloads\*)
+REM 	del %%x\downloads\*
 REM )
 
 :: Root drive garbage
 REM rmdir /S /Q %SystemDrive%\Temp 2>NUL
 REM for %%i in (bat,txt,log,jpg,jpeg,tmp,bak,backup,exe) do (
-REM			del /F /Q "%SystemDrive%\*.%%i" 2>NUL
-REM	 )
+REM	del /F /Q "%SystemDrive%\*.%%i" 2>NUL
+REM )
 
 :: System temp files
 del /F /S /Q "%WINDIR%\TEMP\*" 2>NUL
 
 :: Remove files left over from installing Nvidia/ATI/AMD/Dell/Intel/HP drivers
 for /D %%i in (NVIDIA,ATI,AMD,Dell,Intel,HP) do (
-			rmdir /S /Q "%SystemDrive%\%%i" 2>NUL
-		)
+	rmdir /S /Q "%SystemDrive%\%%i" 2>NUL
 	)
-:: Clear additional unneeded files from NVIDIA driver installs
-if exist "%ProgramFiles%\Nvidia Corporation\Installer2" rmdir /s /q "%ProgramFiles%\Nvidia Corporation\Installer2"
-if exist "%ALLUSERSPROFILE%\NVIDIA Corporation\NetService" del /f /q "%ALLUSERSPROFILE%\NVIDIA Corporation\NetService\*.exe"
+)
 
+:: Clear additional unneeded files from NVIDIA driver installs
+if exist "%ProgramFiles%\Nvidia Corporation\Installer2" (
+	rmdir /s /q "%ProgramFiles%\Nvidia Corporation\Installer2"
+)
+if exist "%ALLUSERSPROFILE%\NVIDIA Corporation\NetService" ( 
+	del /f /q "%ALLUSERSPROFILE%\NVIDIA Corporation\NetService\*.exe"
+)
 :: Clear Windows Search Temp Data + Index
-if exist "%ALLUSERSPROFILE%\Microsoft\Search\Data\Temp" rmdir /s /q "%ALLUSERSPROFILE%\Microsoft\Search\Data\Temp"
+if exist "%ALLUSERSPROFILE%\Microsoft\Search\Data\Temp" (
+	rmdir /s /q "%ALLUSERSPROFILE%\Microsoft\Search\Data\Temp"
+)
 echo.
 echo Stopping search service to wipe search data and index...
 echo.
@@ -234,21 +242,28 @@ net stop Wsearch
 echo.
 if exist %SystemDrive%\ProgramData\Microsoft\Search\Data\Applications\Windows (
 	del /F /S /Q %SystemDrive%\ProgramData\Microsoft\Search\Data\Applications\Windows\*
-	)
 )
 echo.
 net start Wsearch
 echo.
 
 :: Remove the Office installation cache.
-if exist %SystemDrive%\MSOCache rmdir /S /Q %SystemDrive%\MSOCache
+if exist %SystemDrive%\MSOCache (
+	rmdir /S /Q %SystemDrive%\MSOCache
+)
 
 :: Remove the Windows installation cache.
-if exist %SystemDrive%\i386 rmdir /S /Q %SystemDrive%\i386
+if exist %SystemDrive%\i386 (
+	rmdir /S /Q %SystemDrive%\i386
+)
 
 :: Empty all recycle bins in XP and up
-if exist %SystemDrive%\RECYCLER rmdir /s /q %SystemDrive%\RECYCLER
-if exist %SystemDrive%\$Recycle.Bin rmdir /s /q %SystemDrive%\$Recycle.Bin
+if exist %SystemDrive%\RECYCLER (
+	rmdir /s /q %SystemDrive%\RECYCLER
+)
+if exist %SystemDrive%\$Recycle.Bin (
+	rmdir /s /q %SystemDrive%\$Recycle.Bin
+)
 
 :: Delete Recycle Bins on other drives
 REM for /D %%N in (D E F G H I J K L M O P Q R S T U V W X Y Z) DO (
@@ -258,37 +273,62 @@ REM IF EXIST %%N\$Recycle.Bin del %%N\$Recycle.Bin\*.* )
 reg delete "HKCU\SOFTWARE\Classes\Local Settings\Muicache" /f
 
 :: Clear Windows Error Reporting
-if exist "%ALLUSERSPROFILE%\Microsoft\Windows\WER\ReportArchive" rmdir /s /q "%ALLUSERSPROFILE%\Microsoft\Windows\WER\ReportArchive"
-if exist "%ALLUSERSPROFILE%\Microsoft\Windows\WER\ReportQueue" rmdir /s /q "%ALLUSERSPROFILE%\Microsoft\Windows\WER\ReportQueue"
+if exist "%ALLUSERSPROFILE%\Microsoft\Windows\WER\ReportArchive" (
+	rmdir /s /q "%ALLUSERSPROFILE%\Microsoft\Windows\WER\ReportArchive"
+)
+if exist "%ALLUSERSPROFILE%\Microsoft\Windows\WER\ReportQueue" (
+	rmdir /s /q "%ALLUSERSPROFILE%\Microsoft\Windows\WER\ReportQueue"
+)
 
 :: Clear Windows Defender Scan Results
-if exist "%ALLUSERSPROFILE%\Microsoft\Windows Defender\Scans\History\Results\Quick" rmdir /s /q "%ALLUSERSPROFILE%\Microsoft\Windows Defender\Scans\History\Results\Quick"
-if exist "%ALLUSERSPROFILE%\Microsoft\Windows Defender\Scans\History\Results\Resource" rmdir /s /q "%ALLUSERSPROFILE%\Microsoft\Windows Defender\Scans\History\Results\Resource"
+if exist "%ALLUSERSPROFILE%\Microsoft\Windows Defender\Scans\History\Results\Quick" (
+	rmdir /s /q "%ALLUSERSPROFILE%\Microsoft\Windows Defender\Scans\History\Results\Quick"
+)
+if exist "%ALLUSERSPROFILE%\Microsoft\Windows Defender\Scans\History\Results\Resource" (
+	rmdir /s /q "%ALLUSERSPROFILE%\Microsoft\Windows Defender\Scans\History\Results\Resource"
+)
 
 :: Clear Windows update logs
-if exist %WINDIR%\*.log del /F /Q %WINDIR%\*.log 2>NUL
-if exist %WINDIR%\*.txt del /F /Q %WINDIR%\*.txt 2>NUL
+if exist %WINDIR%\*.log (
+	del /F /Q %WINDIR%\*.log 2>NUL
+)
+if exist %WINDIR%\*.txt (
+	del /F /Q %WINDIR%\*.txt 2>NUL
+)
 
 :: Because that guided tour thing is annoying
 REM if /i "%WIN_VER:~0,9%"=="Microsoft" (
-REM	if exist %WINDIR%\system32\dllcache\tourstrt.exe del %WINDIR%\system32\dllcache\tourstrt.exe 2>NUL
-REM	if exist %WINDIR%\system32\dllcache\tourW.exe del %WINDIR%\system32\dllcache\tourW.exe 2>NUL
-REM	if exist %WINDIR%\Help\Tours rmdir /S /Q %WINDIR%\Help\Tours 2>NUL
+REM	if exist %WINDIR%\system32\dllcache\tourstrt.exe (
+REM		del %WINDIR%\system32\dllcache\tourstrt.exe 2>NUL
+REM	)
+REM	if exist %WINDIR%\system32\dllcache\tourW.exe 
+REM		del %WINDIR%\system32\dllcache\tourW.exe 2>NUL
+REM	)
+REM	if exist %WINDIR%\Help\Tours (
+REM		rmdir /S /Q %WINDIR%\Help\Tours 2>NUL
 REM	)
 
 :: Windows Server: remove built-in media files (all Server versions)
 REM echo %WIN_VER% | findstr /i /c:"server" >NUL
 REM if %ERRORLEVEL%==0 do (
-REM		if exist %WINDIR%\Media takeown /f %WINDIR%\Media /r /d y 2>NUL && echo.
-REM		if exist %WINDIR%\Media icacls %WINDIR%\Media /grant administrators:F /t  && echo.
-REM		if exist %WINDIR%\Media rmdir /S /Q %WINDIR%\Media 2>NUL )
-REM
+REM 	if exist %WINDIR%\Media (
+REM		takeown /f %WINDIR%\Media /r /d y 2>NUL && echo.
+REM	)
+REM	if exist %WINDIR%\Media (
+REM		icacls %WINDIR%\Media /grant administrators:F /t  && echo.
+REM	)
+REM	if exist %WINDIR%\Media (
+REM		rmdir /S /Q %WINDIR%\Media 2>NUL
+REM	)
+REM )
 
 :: CBS Log cleaner
-if exist %WINDIR%\Logs\CBS del /F /Q %WINDIR%\Logs\CBS\* 2>NUL
+if exist %WINDIR%\Logs\CBS (
+	del /F /Q %WINDIR%\Logs\CBS\* 2>NUL
+)
 echo.
 
-:End
+:end
 echo -----------------------------------------------------------
 echo Cleanup started at %starttime% and finished at %TIME%
 echo -----------------------------------------------------------
