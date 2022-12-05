a6dfac8eeb98f9977480e43b45757a64375e5f01
sawft99 <81699231+sawft99@users.noreply.github.com>
2022-12-04 20:54:28 -0500

    Add GitRebase.bat
    Will rebase a repo

---
+++ GitRebase.bat
@@ -0,0 +1,17 @@
+rem Repo location
+set repo=C:\PathToRepo\.git
+cd /d %repo%
+rem Create and switch to new branch "tmp"
+git checkout --orphan tmp
+rem Add files
+git add -A
+rem Commit to new branch
+git commit -am "Init"
+rem Delete "main" branch
+git branch -D main
+rem Rename "tmp" branch to "main"
+git branch -m main
+rem Update repository
+git push -f origin main
+echo.
+pause
